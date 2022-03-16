9031908399bf22672d90d3313792d5faf97cdfd4
talBrau <tal.brauner@mail.huji.ac.il>
2022-03-16 12:47:13 +0200

    commented out needed parts

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -22,22 +22,22 @@ def test_univariate_gaussian():
         X.fit(m_samples)
         estimated_mean.append(np.abs(X.mu_ - mu))
 
-    # go.Figure([go.Scatter(x=ms, y=estimated_mean, mode='markers+lines', name=r'$\widehat\mu$')]
-    #           ,layout=go.Layout(title =r"$\text{ Estimation of Expectation As Function Of Number Of Samples}$",
-    #                             xaxis_title="$m\\text{ - number of samples}$",
-    #                             yaxis_title="r$\hat\mu$",
-    #                             height=500)).show()
+    go.Figure([go.Scatter(x=ms, y=estimated_mean, mode='markers+lines', name=r'$\widehat\mu$')]
+              ,layout=go.Layout(title =r"$\text{ Estimation of Expectation As Function Of Number Of Samples}$",
+                                xaxis_title="$m\\text{ - number of samples}$",
+                                yaxis_title="r$\hat\mu$",
+                                height=500)).show()
 
 
     # Question 3 - Plotting Empirical PDF of fitted model
     pdfAtSamples = uniGaus.pdf(S)
 
-    # go.Figure([go.Scatter(x = S,y=pdfAtSamples, mode='markers', name='Empirical Pdf Values')]
-    #           ,layout=go.Layout(title=r"$\text{ Empirical Pdf Function Under fitted Uni variate model}$",
-    #                             xaxis_title="$\\text{Samples}$",
-    #                             yaxis_title="$\\text{Pdf}$",
-    #                             showlegend=True,
-    #                             height=500)).show()
+    go.Figure([go.Scatter(x = S,y=pdfAtSamples, mode='markers', name='Empirical Pdf Values')]
+              ,layout=go.Layout(title=r"$\text{ Empirical Pdf Function Under fitted Uni variate model}$",
+                                xaxis_title="$\\text{Samples}$",
+                                yaxis_title="$\\text{Pdf}$",
+                                showlegend=True,
+                                height=500)).show()
 
 
 
@@ -53,22 +53,17 @@ def test_multivariate_gaussian():
     S = np.random.multivariate_normal(means,covMat,1000)
     X = MultivariateGaussian()
     X.fit(S)
-    # print(X.mu_)
-    # print(X.cov_)
+    print(X.mu_)
+    print(X.cov_)
 
     # Question 5 - Likelihood evaluation
     f1 = np.linspace(-10,10,200)
     f3 = np.linspace(-10,10,200)
 
-    # likelyHoods = []
-    # for i in f1:
-    #     for j in f3:
-    #         likelyHoods.append(MultivariateGaussian.log_likelihood([i,0,j,0], covMat, S))
-
     likelyHoods = [[(MultivariateGaussian.log_likelihood([i, 0, j, 0], covMat, S)) for i in f1]for j in f3]
     fig = go.Figure(data=go.Heatmap(x = f1, y = f3,z= likelyHoods),
                     layout=go.Layout(title = 'Heatmap Represtentation of Log Liklyhood'))
-    # fig.show()
+    fig.show()
 
     # Question 6 - Maximum likelihood
     i_max,j_max = np.unravel_index(np.argmax(likelyHoods),np.shape(likelyHoods))
@@ -78,5 +73,5 @@ def test_multivariate_gaussian():
 
 if __name__ == '__main__':
     np.random.seed(0)
-    # test_univariate_gaussian()
+    test_univariate_gaussian()
     test_multivariate_gaussian()
